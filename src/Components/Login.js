0c692c0e67fd1f6d9afaa957d74567a9d6e308c6
Bastien Durand <basilexpeditions@gmail.com>
2022-08-09 16:57:59 +1000

    added error handling logic

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
 import './Login.css';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [emailError, setEmailError] = useState('');
+
+  const handleLogin = () => {
+    if (password.length < 1) {
+      setError('Enter a password');
+    } else if (password.length < 5) {
+      setError('Password is too short');
+    }
+
+    if (email.length < 1) {
+      setEmailError('Enter valid Email');
+    }
+    console.log(email);
+    console.log(password);
+  };
+
+  const handleReset = () => {
+    window.alert('Password reset request processing');
+  };
+
   return (
-    <div className='section'>
+    <form className='section'>
       <Card sx={{ minWidth: 275 }}>
         <Box
           component='form'
@@ -21,22 +43,52 @@ const Login = () => {
           <div>
             <TextField
               required
+              error={emailError.length > 1}
+              helperText={emailError}
               id='outlined-required'
               label='Email'
               defaultValue=''
+              value={email || ''}
+              onChange={(e) => {
+                if (emailError.length > 0) {
+                  setEmailError('');
+                }
+                setEmail(e.target.value);
+              }}
             />
           </div>
+
           <div>
             <TextField
               required
-              id='outlined-required'
+              error={error.length > 1}
+              helperText={error}
+              id='outlined-password-input'
               label='Password'
-              defaultValue=''
+              type='password'
+              value={password || ''}
+              onChange={(e) => {
+                if (error.length > 0) {
+                  setError('');
+                }
+                setPassword(e.target.value);
+              }}
+              autoComplete='current-password'
             />
           </div>
         </Box>
+        <div className='loginbutton'>
+          <Button variant='contained' onClick={handleLogin}>
+            Login
+          </Button>
+        </div>
+        <div className='resetbutton'>
+          <Button variant='outlined' onClick={handleReset}>
+            Reset Password
+          </Button>
+        </div>
       </Card>
-    </div>
+    </form>
   );
 };
 
