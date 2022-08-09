931d7ad760a62d8a879ba6730ea429bfd5ddd7b6
Bastien Durand <basilexpeditions@gmail.com>
2022-08-09 16:07:04 +1000

    added static is authenticated + conditional render

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Login from './Components/Login';
+import Homepage from './Components/Homepage';
 import './App.css';
 
 function App() {
+  const isAuthenticated = false;
+
   return (
-    <div className='App'>
-      <Login />
-    </div>
+    <div className='App'>{isAuthenticated ? <Homepage /> : <Login />}</div>
   );
 }
 
